f233dca21f887dc0a4cb02386160a7c3ada05cad
vu giap <vudinhgiap11@gmail.com>
2023-07-06 10:14:55 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemtime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localdate = DateTime.Now;
+            Console.WriteLine(" Datetime Now is: " + localdate);
+        }
+    }
+}
